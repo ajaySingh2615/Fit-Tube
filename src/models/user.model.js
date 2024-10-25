efd00ebd644c25f40bb253ae610e4b18e62deef7
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-25 19:20:20 +0530

    logic building - register user done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrpt.hash(this.password, 10);
+  this.password = await bcrpt.hash(this.password, 10);
   next();
 });
 
